a76a919a62e4208d30cb54f7c278f830d7f0b53c
Saad_Naseem <167574691+saad-rao@users.noreply.github.com>
2024-11-16 17:17:59 +0500

    Add files via upload

---
+++ JS Assignment 1 to 3/index.js
@@ -0,0 +1,40 @@
+
+                    //   ALERT
+
+
+// var name1=('my name is saad naseem')
+// alert(name1)
+
+// var email = "saadnaseem123@gmail.com"
+// alert("My email address is" + ' '+ email)
+
+
+// var book = "A smarter way to learn javascriipt"
+// alert("I am trying to learn from the book"+' '+book)
+
+
+                      //    VARIABLE FOR STRING
+
+// var myName = ('My name is Saad naseem')
+// document.write(myName);
+
+
+
+                       // variable for number
+
+
+//  var num1=10
+//  var num2=10
+//  document.write(num1+num2)                       
+
+// var age = 20;
+// alert(age)
+
+// var birthYear= 2004;
+// document.write("My birth year is"+' '+ birthYear  ,"<br>" ,"data type of my declared variable is number" ,"</br>")
+
+
+// var visitorName = "johndoe";
+// var productTitle="T-shirt(s)";
+// var quantity =5;
+// document.write("<b>" , visitorName +' '+ "</b>",  "ordered " , "<b>", quantity +' '+ "</b>" ,"<b>", productTitle ,"</b>" +' '+ "on Diners clothing store" )
