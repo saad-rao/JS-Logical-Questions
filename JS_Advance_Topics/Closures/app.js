3435a6f501f8ddcdef4f96e700719d2a8298397d
Saad_Naseem <raosad786@gmail.com>
2025-02-06 19:17:36 +0500

    Add click counter functionality

--- JS_Advance_Topics/Closures/app.js
+++ JS_Advance_Topics/Closures/app.js
@@ -6,28 +6,28 @@
 //  Event listeners
 //  Memoization & Optimization
 
-
 // document.write(`-------------------------QS:1--------------------------` ,"<br>","<br>")
 
 // 1. Counter Function (Encapsulation of Data)
-//Create a function createCounter that returns a function. 
+//Create a function createCounter that returns a function.
 //The returned function should increment and return a counter
 //every time it is called.
 
-function createCounter (){
-    let count = 0
+function createCounter() {
+  let count = 0;
 
-    return function (){
-        count++
-        return count
-    }
+  return function () {
+    count++;
+    return count;
+  };
 }
 
-let counter = createCounter()
+let counter = createCounter();
 console.log(counter());
 console.log(counter());
 console.log(counter());
 
+// document.write(`-------------------------QS:2--------------------------` ,"<br>","<br>")
 
 // 2. Private Data (Data Hiding)
 
@@ -35,20 +35,41 @@ console.log(counter());
 // and returns an inner function that allows getting and changing
 // the username securely.
 
-function userAccount (username){
-   return{
-    getUsername:function(){
-        return username
+function userAccount(username) {
+  return {
+    getUsername: function () {
+      return username;
     },
 
-    setUsername:function(newName){
-username=newName
-    }
-   }
+    setUsername: function (newName) {
+      username = newName;
+    },
+  };
 }
 
-let user = userAccount("Saad Naseem")
+let user = userAccount("Saad Naseem");
 console.log(user.getUsername());
 
-user.setUsername("Saad")
+user.setUsername("Saad");
 console.log(user.getUsername());
+
+// document.write(`-------------------------QS:3--------------------------` ,"<br>","<br>")
+
+// 3. Button Click Counter
+
+// Write a function clickCounter that keeps track of how many times a button is clicked.
+
+function clickCounter() {
+  let count = 0;
+  return function () {
+    count++;
+
+    document.getElementById("output").innerHTML = "clicks : " + count;
+  };
+}
+
+let countClicks = clickCounter();
+document.getElementById("btn").addEventListener("click", countClicks);
+
+// When you click the button, the result will update (e.g., "Clicks: 1", "Clicks: 2").
+// The button will not be removed; only the count below it will be updated.
