927cd448380aded8b793b4c5f3d8835ab33955a8
Saad_Naseem <raosad786@gmail.com>
2025-02-11 23:06:10 +0500

    Add questions and examples for JavaScript Template Literals

---
+++ JS_Advance_Topics/Template_Literals/app.js
@@ -0,0 +1,8 @@
+                               // Questions on JavaScript Template Literals
+// Q1: String Interpolation
+// Create a template literal that dynamically inserts a user's name and age into a sentence.
+
+let name = "Saad";
+let age = 20;
+let message = `Hello, my name is ${name} and I am ${age} years old`;
+console.log(message);
