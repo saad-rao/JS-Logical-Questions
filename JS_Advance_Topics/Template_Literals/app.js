3d4d0178e263c8f78515ad6cff630ffecf88a6d9
Saad_Naseem <raosad786@gmail.com>
2025-02-11 23:29:04 +0500

    Add a question

--- JS_Advance_Topics/Template_Literals/app.js
+++ JS_Advance_Topics/Template_Literals/app.js
@@ -6,3 +6,12 @@ let name = "Saad";
 let age = 20;
 let message = `Hello, my name is ${name} and I am ${age} years old`;
 console.log(message);
+
+
+// Q2: Expression Evaluation in Template Literals
+// Calculate the area of a rectangle dynamically using template literals.
+
+const length = 10;
+const width = 5;
+const areaMessage = `The area of the rectangle is ${length*width} square units`
+console.log(areaMessage);
