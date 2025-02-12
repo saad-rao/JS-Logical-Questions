debf95fcac8c6e513ade2afe7c3a33f9616e85e8
Saad_Naseem <raosad786@gmail.com>
2025-02-12 22:36:11 +0500

    Add destructuring example

---
+++ JS_Advance_Topics/Destructuring/app.js
@@ -0,0 +1,14 @@
+// Q1: Basic Object Destructuring
+// Extract the name and age properties from the person object using destructuring.
+
+const person = {
+    name: "Saad",
+    age: 20,
+    city: "Karachi"
+};
+
+let name = person.name
+let age = person.age
+
+console.log(name);
+console.log(age); 
