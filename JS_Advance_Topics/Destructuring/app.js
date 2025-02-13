d9950c17d9335110b26f424e42b1290287962b86
Saad_Naseem <raosad786@gmail.com>
2025-02-13 23:17:33 +0500

    Add question for variable renaming in destructuring

--- JS_Advance_Topics/Destructuring/app.js
+++ JS_Advance_Topics/Destructuring/app.js
@@ -12,3 +12,20 @@ let age = person.age
 
 console.log(name);
 console.log(age); 
+
+
+
+// Q2: Renaming Variables While Destructuring
+// Extract firstName as fName and lastName as lName from the user object.
+
+
+let user = {
+    firstName: "Saad",
+    lastName: "Naseem"
+};
+
+let fName = user.firstName
+let lName = user.lastName
+
+console.log(fName); 
+console.log(lName); 
