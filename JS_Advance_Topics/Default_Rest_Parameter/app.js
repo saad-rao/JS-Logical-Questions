c9587bd4424541a1f5437139448572b2ac92ab70
Saad_Naseem <raosad786@gmail.com>
2025-02-15 23:09:43 +0500

    Add default parameter QS

---
+++ JS_Advance_Topics/Default_Rest_Parameter/app.js
@@ -0,0 +1,10 @@
+// Q1: Basic Default Parameter
+// Create a function greet that takes a name parameter and has a default value of "Guest" if no name is provided.
+
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+greet();       
+greet("Saad"); 
